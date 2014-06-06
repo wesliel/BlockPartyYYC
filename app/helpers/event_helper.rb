523477064c3dcbc557667f345d5f89a39f91e40b
Weslie Leung <wleung@madebyuppercut.com>
2014-06-06 16:48:52 -0600

    Tweak sharing copy

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -1,2 +1,30 @@
 module EventHelper
+	def event_twitter_name
+		@event.event_type == "Other" ? "" : " ##{@event.event_type}"
+	end
+
+	def event_counter(is_plural)
+		return_string = ""
+
+		case @event.event_type
+			when "BBQ"
+				return is_plural ? "#parties" : "#party"
+			when "Potluck"
+				return is_plural ? "#parties" : "#party"
+			when "Catered"
+				return is_plural ? "#parties" : "#party"
+			when "Picnic"
+				return is_plural ? "#parties" : "#party"
+			when "Community"
+				return is_plural ? "#events" : "#event"
+			when "Fundraiser"
+				return is_plural ? "#events" : "#event"
+			when "Other"
+				return is_plural ? "#events" : "#event"
+		end
+	end
+
+	def hash_tag
+		(["2014-06-20", "2014-06-21", "2014-06-22"].include? @event.date) == true ? " #NeighbourDay #YYC" : " #YYC"
+	end
 end
